d5561af7bdfb3a9c12fe08d4f024ae9ce7ad8cfc
unknown <yamiteka@gmail.com>
2020-09-22 15:32:38 +0300

    первый

---
+++ Practice/03/C++ 03/practis03/practis03.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+
+{
+	setlocale(LC_ALL, "Russian");
+	int a, b;
+	cout << "Введите переменную a" << endl;
+	cin >> a;
+	cout << "ВВедите переменную b" << endl;
+	cin >> b;
+
+	cout << "a+b=" << a + b << endl;
+	cout << "a-b=" << a - b << endl;
+	cout << "a*b=" << a * b << endl;
+	cout << "a/b=" << a / b << endl;
+
+
+
+   
+}
+
