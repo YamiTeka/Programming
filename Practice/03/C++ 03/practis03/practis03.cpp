140e80915c2ac19c37d3859fd58fb67b00564ddd
Dasha Makurina <yamiteka@gmail.com>
2020-10-29 15:46:59 +0300

    master

--- Practice/03/C++ 03/practis03/practis03.cpp
+++ Practice/03/C++ 03/practis03/practis03.cpp
@@ -11,10 +11,52 @@ int main()
 	cout << "ВВедите переменную b" << endl;
 	cin >> b;
 
-	cout << "a+b=" << a + b << endl;
-	cout << "a-b=" << a - b << endl;
-	cout << "a*b=" << a * b << endl;
-	cout << "a/b=" << a / b << endl;
+	cout << "a+b=" << a + b ;
+	cout << "    a-b=" << a - b << endl;
+	cout << "a*b=" << a * b ;
+	cout << "    a/b=" << a / b << endl;
+
+	double c,d;
+	cout << "Введите переменную c" << endl;
+	cin >> c;
+	cout << "ВВедите переменную d" << endl;
+	cin >> d;
+
+	cout << "c+d=" << c + d;
+	cout << "    c-d=" << c - d << endl;
+	cout << "c*d=" << c * d;
+	cout << "    c/d=" << c / d << endl; 
+
+
+	int i;
+	double f;
+	cout << "Введите переменную i" << endl;
+	cin>> i;
+	cout << "ВВедите переменную f" << endl;
+	cin >>f;
+
+	cout << "i+f=" << i +f;
+	cout << "    i-f=" << i - f << endl;
+	cout << "i*f=" << i * f;
+	cout << "    i/f=" << i / f << endl;
+
+	int h;
+	double g;
+	cout << "Введите переменную g" << endl;
+	cin >> g;
+	cout << "ВВедите переменную h" << endl;
+	cin >> h;
+
+	cout << "g+h=" <<  g + h;
+	cout << "    g-h=" << g - h << endl;
+	cout << "g*h=" << g * h;
+	cout << "    g/h=" << g / h << endl;
+
+
+	
+
+
+
 
 
 
