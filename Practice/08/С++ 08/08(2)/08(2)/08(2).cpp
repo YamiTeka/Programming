5aa5077d62f7441f1e7479c0ccc85b0929cfbeab
Dasha Makurina <yamiteka@gmail.com>
2020-10-29 22:55:54 +0300

    master

--- Practice/08/С++ 08/08(2)/08(2)/08(2).cpp
+++ Practice/08/С++ 08/08(2)/08(2)/08(2).cpp
@@ -6,20 +6,25 @@ int main()
 	setlocale(LC_ALL, "Russian");
 	int a, b, c;
 	char z;
-	cout << "Введите число-действие-число";
+	cout << "Введите число-действие-число"<< endl;
 	cin >> a >> z >> b;
 	switch (z) {
-		case '+':
-			c = a + b;
-			cout << a << "+" << b << "=" << c; 
-		case '-':
-			c = a - b;
-			cout << a << "-" << b << "=" << c; 
-		 case '*':
-	        c = a * b;
-	        cout << a << "*" << b << "=" << c; 
-		case '/':
-			c = a / b;
-			cout << a << "/" << b << "=" << c; 
+	case '+':
+	{c = a + b;
+	cout << a << "+" << b << "=" << c;
+	break;}
+	case '-':
+	{c = a - b;
+	cout << a << "-" << b << "=" << c; 
+	break;}
+	case '*':
+	{c = a * b;
+	cout << a << "*" << b << "=" << c; 
+	break;}
+	case '/':
+	{c = a / b;
+	cout << a << "/" << b << "=" << c; 
+	break;}
+	}
 
 }
