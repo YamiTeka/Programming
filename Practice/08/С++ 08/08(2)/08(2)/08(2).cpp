1531f8872bab6809b55317e1e30c00c0f41e8e7b
Dasha Makurina <yamiteka@gmail.com>
2020-10-29 15:59:22 +0300

    master

---
+++ Practice/08/С++ 08/08(2)/08(2)/08(2).cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a, b, c;
+	char z;
+	cout << "Введите число-действие-число";
+	cin >> a >> z >> b;
+	switch (z) {
+		case '+':
+			c = a + b;
+			cout << a << "+" << b << "=" << c; 
+		case '-':
+			c = a - b;
+			cout << a << "-" << b << "=" << c; 
+		 case '*':
+	        c = a * b;
+	        cout << a << "*" << b << "=" << c; 
+		case '/':
+			c = a / b;
+			cout << a << "/" << b << "=" << c; 
+
+}
