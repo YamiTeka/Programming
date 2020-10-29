a799642785562127baa2fbee3d923d24155a9350
Dasha Makurina <yamiteka@gmail.com>
2020-10-29 15:54:12 +0300

    master

---
+++ Practice/04/С++ 04/04/04/04.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double a, b;
+	cout << "Введите значение переменной a и b" << endl;
+	cin >> a>> b;
+	 cout << "Ввывод без дополнительной переменной" << endl;
+	a = a + b;
+	b = b - a;
+	b = -b;
+	a = a - b;
+	cout << "a=" << a << " b=" << b << endl; 
+	cout << "Ввывод с дополнительной переменной" << endl;
+	double y,z,x;
+	cin >> x>> y;
+	z = x;
+	x = y;
+	y = z;
+	cout << "x=" << x << " y=" << y;
+	
+
+}
---
+++ Practice/05/С++ 05/05/05.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	// для X0,V0 и t выбираем пременную double т.к эти значения могут быть не целыми числами 
+	double X0,V0,t;
+	double a;
+	double g = 9.8;
+	a = g;
+	cout << "Введите значение переменных через пробел x0, V0,t" << endl;
+	cin >> X0 >> V0 >> t;
+	
+	cout <<"Объект преодолеет растояни в " << X0 + V0 * t - ((a * t * t)/2);
+}
+	
