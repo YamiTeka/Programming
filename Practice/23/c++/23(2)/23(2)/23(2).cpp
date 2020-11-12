8f8ed583ed1be39070a53961e6b941c99f436f43
Dasha Makurina <yamiteka@gmail.com>
2020-11-12 22:33:34 +0300

    23

---
+++ Practice/23/c++/23(2)/23(2)/23(2).cpp
@@ -0,0 +1,8 @@
+﻿#include <Fact.h>
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
---
+++ Practice/23/c++/Fact.h/Fact.h/Fact.h.cpp
@@ -0,0 +1,20 @@
+﻿#ifndef Fact 
+#define Fact
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n, i, res;
+	n = 10;
+	res = 1;
+
+	for (int i = 1; i <= n; ++i)
+	{
+		res = res * i;
+		cout << i << " " << res << endl;
+		cout << " " << endl;
+	}
+
+
+}
+#endif
