d3e1a9c05a95330ffd3e3bf79511d2a935108088
Dasha Makurina <yamiteka@gmail.com>
2020-12-12 22:11:32 +0300

    28

---
+++ Practice/28/C++/28/28/28.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <algorithm>
+#include<vector>
+#include<map>
+using namespace std;
+
+void f(int n)
+{
+    map  < int, int> dict;
+    int div = 2;
+    while (n > 1)
+    {
+        for ( int i = 2; i <= n; i++)
+        {
+            if (!(n % i))
+            {
+                n /= i;
+
+                dict[i] = dict.count(i) ? dict[i] + 1 : 1;
+                break;
+            }
+        }
+    }
+    cout << "1";
+    for (auto now : dict)
+    {
+        if (now.second > 1)
+        {
+            cout << "*" << now.first << "^" << now.second;
+        }
+        else
+        {
+            cout << "*" << now.first;
+        }
+    }
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int n;
+    cout << "Введите значение n";
+    cin >> n;
+    f(n);
+}
